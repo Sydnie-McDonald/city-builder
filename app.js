fa341feb6478eefff29ba800091b78db75af8d21
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-27 18:36:24 -0600

    working

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+/* eslint-disable no-unused-vars */
 // import functions and grab DOM elements
 const cityDropdown = document.getElementById('city-dropdown');
 const styleDropdown = document.getElementById('style-dropdown');
 
 const cityEl = document.getElementById('city');
 const styleEl = document.getElementById('style');
-const sloganEl = document.getElementById('slogan');
-// let state
+const reportEl = document.getElementById('report');
 
+const sloganEl = document.getElementById('slogan');
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+// let state
+let slogans = [];
+let cityCounter = 0;
+let styleCounter = 0;
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+cityDropdown.addEventListener('change', (e) => {
+  const value = e.target.value;
+  cityEl.style.backgroundImage = `url("./assets/${value}-city.png")`;
+  cityCounter++;
+});
+
+/*styleDropdown.addEventListener('change', (e) => {
+  const value = e.target.value;
+  styleEl.style.backgroundImage = `url("./assets/${value}-style.png")`;
+  styleCounter++;
+}); 
